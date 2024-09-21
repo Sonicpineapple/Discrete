bc226aada0ea7d2b4a5f24f96a0b441d71fe9d6e
Luna Harran <36711077+Sonicpineapple@users.noreply.github.com>
2024-09-21 18:28:11 +0100

    Fix mirror computation and implement mirror rendering

--- src/geom.rs
+++ src/geom.rs
@@ -1,6 +1,6 @@
 use core::f64;
 
-use cga2d::{Blade, Blade3};
+use cga2d::prelude::*;
 
 pub(crate) fn rank_3_mirrors(a: usize, b: usize) -> [Blade3; 3] {
     let a1 = f64::consts::PI / a as f64;
@@ -19,15 +19,15 @@ pub(crate) fn rank_4_mirrors(a: usize, b: usize, c: usize) -> [Blade3; 4] {
 
 fn rank_3_mirrors_internal(a1: f64, a2: f64) -> [Blade3; 3] {
     let x_unit = cga2d::point(1., 0.);
-    let mirror1 = cga2d::NO ^ x_unit ^ cga2d::NI;
-    let mirror2 =
-        cga2d::slerp(mirror1.connect(cga2d::NO), !mirror1.connect(cga2d::NO), a1) ^ cga2d::NI;
+    let mirror1 = NO ^ x_unit ^ NI;
+    let mirror2 = (NO << mirror1).rotate(a1) ^ NI;
 
     // this is kind of magic? u is symmetric with the desired mirror3
     let q3 = (x_unit << mirror1) ^ !mirror2;
-    let u = cga2d::slerp(mirror2 & q3, !(mirror2 & q3), a2) ^ x_unit;
-    let vertex_2_3 = (u & mirror1).unpack_point_pair()[0];
-    let mirror3 = !mirror1 ^ x_unit ^ vertex_2_3;
+    let u = (mirror2 & q3).rotate(a2) ^ x_unit;
+    // let vertex_2_3 = (u & mirror1).unpack_point_pair().expect("what")[1];
+    let mirror3 = mirror1.connect(u & mirror1);
+    // let mirror3 = dbg!(!mirror1) ^ dbg!(x_unit) ^ dbg!(vertex_2_3);
     [mirror1, mirror2, mirror3]
 }
 
@@ -41,12 +41,8 @@ fn rank_4_last_mirror_internal(
 ) -> Blade3 {
     let mutual_perpendicular = !(mirror1 & mirror2 & mirror3);
     let temp_angle = (a3.sin() * a1.sin() / a2.cos()).asin();
-    let temp_line = cga2d::slerp(
-        mirror1,
-        !mutual_perpendicular ^ !mirror1 ^ cga2d::NO,
-        -temp_angle,
-    );
-    let vertex_3_4 = (temp_line & mirror3).unpack_point_pair()[1];
+    let temp_line = cga2d::slerp(mirror1, !mutual_perpendicular ^ !mirror1 ^ NO, -temp_angle);
+    let vertex_3_4 = (temp_line & mirror3).unpack_point_pair().expect("what")[1];
     let mirror4 = !mirror1 ^ !mirror2 ^ vertex_3_4;
     mirror4
 }
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,143 @@
-use geom::rank_4_mirrors;
+use cga2d::Multivector;
+use eframe::egui::{self, pos2, Pos2};
+use geom::{rank_3_mirrors, rank_4_mirrors};
 use todd_coxeter::Tables;
 
 mod geom;
 mod group;
 mod todd_coxeter;
 
-fn main() {
-    let gen_count = 4;
+fn main() -> eframe::Result<()> {
+    let native_options = eframe::NativeOptions {
+        follow_system_theme: false,
+        ..Default::default()
+    };
+    eframe::run_native(
+        "Discrete",
+        native_options,
+        Box::new(|cc| Ok(Box::new(App::new(cc)))),
+    )
+}
 
-    // mirrors are assumed self-inverse
-    // let mut rels = schlafli_rels(vec![7, 3]);
-    // rels.push((0..8).flat_map(|_| [0, 2, 1]).collect());
-    let mut rels = schlafli_rels(vec![8, 3, 3]);
-    rels.push((0..2).flat_map(|_| [0, 2, 1, 0, 2, 1, 0, 1]).collect());
+struct App {
+    scale: f32,
+    mirrors: [cga2d::Blade3; 4],
+}
+impl App {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        let gen_count = 4;
 
-    let subgroup = vec![0, 1]; // generators are assumed mirrors
+        // mirrors are assumed self-inverse
+        // let mut rels = schlafli_rels(vec![7, 3]);
+        // rels.push((0..8).flat_map(|_| [0, 2, 1]).collect());
+        let mut rels = schlafli_rels(vec![8, 3, 3]);
+        rels.push((0..2).flat_map(|_| [0, 2, 1, 0, 2, 1, 0, 1]).collect());
 
-    let mut tables = Tables::new(gen_count, &rels, &subgroup);
-    loop {
-        if !tables.discover_next_unknown() {
-            println!("Done");
-            break;
+        let subgroup = vec![0, 1]; // generators are assumed mirrors
+
+        let mut tables = Tables::new(gen_count, &rels, &subgroup);
+        loop {
+            if !tables.discover_next_unknown() {
+                println!("Done");
+                break;
+            }
+        }
+
+        print!("{}", tables.coset_group());
+
+        let mirrors = rank_4_mirrors(8, 3, 3);
+        Self {
+            scale: 0.5,
+            mirrors,
         }
     }
+}
+impl eframe::App for App {
+    fn update(&mut self, ctx: &eframe::egui::Context, frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            let rect = ui.available_rect_before_wrap();
+            let (cen, size) = (rect.center(), rect.size());
+            let unit = size.min_elem() * self.scale / 2.;
 
-    print!("{}", tables.coset_group());
+            // Allocate space in the UI.
+            let (egui_rect, target_size) =
+                rounded_pixel_rect(ui, ui.available_rect_before_wrap(), 1);
+            let r = ui.allocate_rect(egui_rect, egui::Sense::click_and_drag());
 
-    let mirrors = rank_4_mirrors(8, 3, 3);
+            let scale = egui_rect.size() / egui_rect.height();
+            let scale = [scale.x.recip() * self.scale, scale.y.recip() * self.scale];
+
+            let screen_to_egui =
+                |pos: Pos| pos2(pos.x as f32, -pos.y as f32) * unit + cen.to_vec2();
+            let egui_to_screen = |pos: Pos2| {
+                let pos = (pos - cen.to_vec2()) / unit;
+                Pos {
+                    x: pos.x as f64,
+                    y: -pos.y as f64,
+                }
+            };
+
+            ui.painter().circle_filled(
+                screen_to_egui(Pos { x: 1., y: 0. }),
+                5.,
+                egui::Color32::GOLD,
+            );
+            ui.painter().circle_filled(
+                screen_to_egui(Pos { x: 0., y: 1. }),
+                5.,
+                egui::Color32::GOLD,
+            );
+
+            let cols = [
+                egui::Color32::RED,
+                egui::Color32::GREEN,
+                egui::Color32::BLUE,
+                egui::Color32::YELLOW,
+            ];
+            let stroke_width = 1.;
+            for (i, mirror) in self.mirrors.iter().enumerate() {
+                match mirror.unpack(0.01) {
+                    cga2d::LineOrCircle::Line { .. } => {
+                        let pp = *mirror & cga2d::circle(cga2d::NO, 2. * unit as f64);
+                        if pp.mag2() > 0. {
+                            ui.painter().line_segment(
+                                if let Some(pp) = pp.unpack_point_pair() {
+                                    pp.map(|p| {
+                                        let (x, y) = p.unpack_point();
+                                        screen_to_egui(Pos { x, y })
+                                    })
+                                } else {
+                                    todo!()
+                                },
+                                (stroke_width, cols[i]),
+                            );
+                        }
+                    }
+                    cga2d::LineOrCircle::Circle { cx, cy, r } => {
+                        ui.painter().circle_stroke(
+                            screen_to_egui(Pos { x: cx, y: cy }),
+                            (r * unit as f64) as _,
+                            (stroke_width, cols[i]),
+                        );
+                    }
+                };
+            }
+        });
+    }
+}
+
+#[derive(Debug, Default, Copy, Clone)]
+struct Pos {
+    x: f64,
+    y: f64,
+}
+impl From<Pos> for Pos2 {
+    fn from(value: Pos) -> Self {
+        Self {
+            x: value.x as f32,
+            y: value.y as f32,
+        }
+    }
 }
 
 fn schlafli_rels(vals: Vec<u8>) -> Vec<Vec<u8>> {
@@ -38,4 +149,34 @@ fn schlafli_rels(vals: Vec<u8>) -> Vec<Vec<u8>> {
         rels.push((0..val).flat_map(|_| [i as u8, i as u8 + 1]).collect());
     }
     rels
+}
+
+/// Rounds an egui rectangle to the nearest pixel boundary and returns the
+/// rounded egui rectangle, along with its width & height in pixels.
+pub fn rounded_pixel_rect(
+    ui: &egui::Ui,
+    rect: egui::Rect,
+    downscale_rate: u32,
+) -> (egui::Rect, [u32; 2]) {
+    let dpi = ui.ctx().pixels_per_point();
+
+    // Round rectangle to pixel boundary for crisp image.
+    let mut pixels_rect = rect;
+    pixels_rect.set_left((dpi * pixels_rect.left()).ceil());
+    pixels_rect.set_bottom((dpi * pixels_rect.bottom()).floor());
+    pixels_rect.set_right((dpi * pixels_rect.right()).floor());
+    pixels_rect.set_top((dpi * pixels_rect.top()).ceil());
+
+    // Convert back from pixel coordinates to egui coordinates.
+    let mut egui_rect = pixels_rect;
+    *egui_rect.left_mut() /= dpi;
+    *egui_rect.bottom_mut() /= dpi;
+    *egui_rect.right_mut() /= dpi;
+    *egui_rect.top_mut() /= dpi;
+
+    let pixel_size = [
+        pixels_rect.width() as u32 / downscale_rate,
+        pixels_rect.height() as u32 / downscale_rate,
+    ];
+    (egui_rect, pixel_size)
 }
