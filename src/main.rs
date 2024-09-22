41f78da85f233e17ca4bf9969962936216b4dd10
HactarCE <6060305+HactarCE@users.noreply.github.com>
2024-09-22 13:30:05 -0400

    Add web support

--- src/main.rs
+++ src/main.rs
@@ -12,16 +12,64 @@ mod gfx;
 mod group;
 mod todd_coxeter;
 
-fn main() -> eframe::Result<()> {
+/// Native main function
+#[cfg(not(target_arch = "wasm32"))]
+fn main() -> eframe::Result {
+    env_logger::init();
+
     let native_options = eframe::NativeOptions {
         follow_system_theme: false,
         ..Default::default()
     };
+
     eframe::run_native(
         "Discrete",
         native_options,
         Box::new(|cc| Ok(Box::new(App::new(cc)))),
     )
+}
+
+/// Web main function
+#[cfg(target_arch = "wasm32")]
+fn main() {
+    // Redirect `log` message to `console.log` and friends:
+    eframe::WebLogger::init(log::LevelFilter::Debug).ok();
+
+    let web_options = eframe::WebOptions {
+        wgpu_options: eframe::egui_wgpu::WgpuConfiguration {
+            supported_backends: wgpu::Backends::GL, // TODO: can we allow WebGPU as well?
+            ..eframe::egui_wgpu::WgpuConfiguration::default()
+        },
+        ..eframe::WebOptions::default()
+    };
+
+    wasm_bindgen_futures::spawn_local(async {
+        let start_result = eframe::WebRunner::new()
+            .start(
+                "eframe_canvas",
+                web_options,
+                Box::new(|cc| Ok(Box::new(App::new(cc)))),
+            )
+            .await;
+
+        // Remove the loading text and spinner:
+        let loading_text = web_sys::window()
+            .and_then(|w| w.document())
+            .and_then(|d| d.get_element_by_id("loading_text"));
+        if let Some(loading_text) = loading_text {
+            match start_result {
+                Ok(_) => {
+                    loading_text.remove();
+                }
+                Err(e) => {
+                    loading_text.set_inner_html(
+                        "<p> The app has crashed. See the developer console for details. </p>",
+                    );
+                    panic!("Failed to start eframe: {e:?}");
+                }
+            }
+        }
+    });
 }
 
 struct Schlafli(Vec<usize>);
